2c516c7354d03ae94e5b095651e35ff1e275507c
RealSmileyguy <byourfacesmells@aol.com>
2014-02-14 12:28:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-$(document).ready(function(){
-  $('.header').mouseover(function(){
-    $('.header').html('goodbye');
-  });
+var value = 0;
+$(document).ready(function () {
+    $('.header').mouseover(function () {
+        $('.header').html('goodbye');
+    });
+    $('.input_button').click(function(){
+        value = $('.input1').val();
+        $('.header').html(value);
+    });
 });
